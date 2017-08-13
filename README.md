0e542557c4c842bc7f8f57c8df97bd4b84cf432a
Jia-Sin Chen <jarsing@chibuapp.com>
2017-08-13 17:55:20 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+linebot-foodguide
+==
+
+美食指南（LINE Bot範例）
+
+相關網路資源
+--
+
+1. [LINE Business Center](https://business.line.me/auth)
+2. [LINE@ MANAGER](https://admin-official.line.me/)
+3. [開發者文件](https://devdocs.line.me/)
+4. [開發者網站的教學文章](https://developers.line.me/messaging-api/getting-started)
+5. [LINE PHP SDK](https://github.com/line/line-bot-sdk-php)
+6. [Microsoft Azure](https://azure.microsoft.com/zh-tw/free/)
+7. [註冊 Google 帳號](https://accounts.google.com/SignUp?hl=zh-TW)
+8. [Google 雲端硬碟](https://drive.google.com/drive/)
+
+知識庫範例
+--
+
+1. [美食指南 JSON 文件](https://spreadsheets.google.com/feeds/list/10er9ZgEDKvAkHTzweszUUj6E34wuAEwAE4qjQrdVm6Q/od6/public/values?alt=json
+)
+
+開發需求
+--
+
+PHP 5.4 or later
---
+++ bot.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once('line-bot-sdk-tiny/LINEBotTiny.php');
+
+$channelAccessToken = '<your channel access token>';
+$channelSecret = '<your channel secret>';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+
+            $json = file_get_contents('https://spreadsheets.google.com/feeds/list/1tQCaj3LUVwH0tBuPrfBY2dOJuF-qzpYEdOqGdNvJRLc/od6/public/values?alt=json');
+            $data = json_decode($json, true);
+            $result = array();
+
+            foreach ($data['feed']['entry'] as $item) {
+                $keywords = explode(',', $item['gsx$keyword']['$t']);
+
+                foreach ($keywords as $keyword) {
+                    if (strpos($message['text'], $keyword) !== false) {
+                        $candidate = array(
+                            'thumbnailImageUrl' => $item['gsx$photourl']['$t'],
+                            'title' => $item['gsx$title']['$t'],
+                            'text' => $item['gsx$title']['$t'],
+                            'actions' => array(
+                                array(
+                                    'type' => 'uri',
+                                    'label' => '查看詳情',
+                                    'uri' => $item['gsx$url']['$t'],
+                                    ),
+                                ),
+                            );
+                        array_push($result, $candidate);
+                    }
+                }
+            }
+
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text'].'讓我想想喔…',
+                            ),
+                            array(
+                                'type' => 'template',
+                                'altText' => '為您推薦下列美食：',
+                                'template' => array(
+                                    'type' => 'carousel',
+                                    'columns' => $result,
+                                ),
+                            ),
+                            array(
+                                'type' => 'text',
+                                'text' => '這些都超好吃，真心不騙！',
+                            ),
+                            array(
+                                'type' => 'sticker',
+                                'packageId' => '1',
+                                'stickerId' => '2',
+                            ),
+                        ),
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
